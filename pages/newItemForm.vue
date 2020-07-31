da910213c861a06fb1a4ee3bfc6da85b0bbca81b
Patricia Santana Cruz <patriciasantanacruz@gmail.com>
2020-07-31 21:08:43 +0100

    Improveved form for publishing items on the front side

--- pages/newItemForm.vue
+++ pages/newItemForm.vue
@@ -1,42 +1,67 @@
 <template>
   <div>
-    <v-file-input
-      label="Selecciona una imagen"
-      filled
-      prepend-icon="mdi-camera"
-    ></v-file-input>
+    <v-form ref="form">
+      <ImagePicker />
+      <v-text-field type="text" label="Ponle un Título"></v-text-field>
 
-    <v-text-field label="Ponle un Título"></v-text-field>
+      <v-text-field
+        type="text"
+        label="Escribe una breve descripción"
+      ></v-text-field>
 
-    <v-text-field label="Escribe una breve descripción"></v-text-field>
+      <v-select
+        type="text"
+        :items="categories"
+        label="Elige una categoría"
+      ></v-select>
 
-    <v-select :items="categories" label="Elige una categoría"></v-select>
+      <v-select type="text" :items="states" label="Estado"></v-select>
 
-    <v-select :items="states" label="Estado"></v-select>
+      <v-text-field
+        type="number"
+        label="Ponle un precio si quieres. Si lo dejas en blanco vacío, entendemos que quieres hacer un regalo"
+      ></v-text-field>
 
-    <v-text-field
-      label="Ponle un precio si quieres. Si lo dejas en blanco vacío, entendemos que quieres hacer un regalo"
-    ></v-text-field>
-
-    <v-btn class="mr-4" @click="submit">Publicar</v-btn>
+      <v-btn class="mr-4" @click="publishItem">Publicar</v-btn>
+    </v-form>
   </div>
 </template>
 
 <script>
+import ImagePicker from '~/components/ImagePicker'
+
 export default {
+  components: { ImagePicker },
   data: () => ({
-    items: [
+    categories: [
       'Todas las categorías',
       'Libros',
       'Material',
       'Uniformes',
       'Juguetes',
     ],
-    states: ['Bueno', 'Muy bueno', 'A estrenar'],
+    states: ['A estrenar', 'Muy bien', 'Bien'],
+    url: null,
   }),
+  methods: {
+    publishItem() {
+      console.log('publishItem')
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-//TODO
+#preview {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: lightgrey;
+  padding: 20px;
+}
+
+#preview img {
+  max-width: 100%;
+  max-height: 500px;
+}
 </style>
