3985d4d4cb09404cf8329e36e4f76b3d4ba3b27d
Patricia Santana Cruz <patriciasantanacruz@gmail.com>
2020-07-31 20:40:17 +0100

    Implemente custome image picker component

---
+++ components/ImagePicker.vue
@@ -0,0 +1,68 @@
+<template>
+  <div
+    class="base-image-input"
+    :style="{ 'background-image': `url(${imageData})` }"
+    @click="chooseImage"
+  >
+    <span v-if="!imageData" class="placeholder">
+      <v-icon x-large>mdi-camera-plus-outline</v-icon>
+    </span>
+    <input
+      ref="fileInput"
+      class="file-input"
+      type="file"
+      @input="onSelectFile"
+    />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return { imageData: null }
+  },
+  methods: {
+    chooseImage() {
+      this.$refs.fileInput.click()
+    },
+    onSelectFile() {
+      const input = this.$refs.fileInput
+      const files = input.files
+      if (files && files[0]) {
+        const reader = new FileReader()
+        reader.onload = (e) => {
+          this.imageData = e.target.result
+        }
+        reader.readAsDataURL(files[0])
+        this.$emit('input', files[0])
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+.base-image-input {
+  display: block;
+  width: 200px;
+  height: 200px;
+  cursor: pointer;
+  background-size: cover;
+  background-position: center center;
+}
+.placeholder {
+  background: #f0f0f0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #333;
+  font-size: 18px;
+}
+.placeholder:hover {
+  background: #e0e0e0;
+}
+.file-input {
+  display: none;
+}
+</style>
